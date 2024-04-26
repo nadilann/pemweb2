774d86c165c2f06200b34429e0437c8142608df6
nadilann <146118871+nadilann@users.noreply.github.com>
2024-04-26 10:45:03 +0700

    Delete praktik2 directory

--- praktik2/submit.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-echo "<h1>Selamat Datang Sania</h1>";
--- praktik2/submit_get.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$username = $_GET['username'];
-$password = $_GET['password'];
-
-echo "<h1>Selamat Datang $username</h1>";
